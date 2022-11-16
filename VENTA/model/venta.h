12c0c14996cead157385ed0f2f1fd10761e30470
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2022-11-16 14:50:51 -0500

    venta.h - Modi. para el CodVenta
    Se ha modificado los parametros del constructor "Venta" quitando el int serie reemplazandolo por el int codigoVenta.

--- VENTA/model/venta.h
+++ VENTA/model/venta.h
@@ -9,19 +9,15 @@ private:
 	int codigoVenta;
 	int codigoCliente;
 	string fecha;
-	int serie;
 	float total;
-	string vendedor;
 public:
 	Venta(){}
-	Venta(int codigoVenta, int codigoCliente, string fecha, int serie, float total, string vendedor)
+	Venta(int codigoVenta, int codigoCliente, string fecha, float total)
 	{
 		this->codigoVenta = codigoVenta;
 		this->codigoCliente = codigoCliente;
 		this->fecha = fecha;
-		this->serie = serie;
 		this->total = total;
-		this->vendedor = vendedor;
 	}
 	
 	void setFecha(string fecha)
@@ -44,18 +40,8 @@ public:
 		return this->fecha;
 	}
 	
-	int getSerieVen()
-	{
-		return this->serie;
-	}
-	
 	float getTotalVen()
 	{
 		return this->total;
-	}
-	
-	string getVendedor()
-	{
-		return this->vendedor;
 	}
 };
