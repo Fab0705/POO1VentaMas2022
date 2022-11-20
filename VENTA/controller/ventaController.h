f606069565b6b4285f3ea0f17b170fe2c93ff0c3
IssaelR <92474827+IssaelR@users.noreply.github.com>
2022-11-19 21:42:11 -0500

    Update ventaController.h

--- VENTA/controller/ventaController.h
+++ VENTA/controller/ventaController.h
@@ -3,6 +3,14 @@
 #include<iostream>
 #include<fstream>
 #include"../model/venta.h"
+#include"../../CLIENTES/controller/clienteController.h"
+
+void menuDeVentas();
+void listarVentas();
+void buscarVenta();
+void eliminarVenta();
+string dameNomCli(int);
+string generarSerie(int);
 
 using namespace std;
 
@@ -43,6 +51,23 @@ public:
 		}
 	}
 	
+	int getPostArray(Venta obj)
+	{
+		for(int i=0;i<vectorVenta.size();i++)
+		{
+			if(obj.getCodVenta() == vectorVenta[i].getCodVenta())	
+			{
+				return i;
+			}
+		}		
+		return -1;
+	}
+	
+	void remove(Venta obj)
+	{
+		vectorVenta.erase(vectorVenta.begin() + getPostArray(obj));
+	}
+	
 	Venta getBuscarPorCodigo(int codigo, int inferior, int superior)
 	{
 		int central;
@@ -70,7 +95,7 @@ public:
 			archivoVenta.open("venta.csv",ios::app);
 			if(archivoVenta.is_open())
 			{
-				archivoVenta<<obj.getCodVenta()<<";"<<obj.getCodCliVen()<<";"<<obj.getFechaVen()<<";"<<obj.getTotalVen()<<";"<<obj.getVendedor()<<";"<<endl;
+				archivoVenta<<obj.getCodVenta()<<";"<<obj.getCodCliVen()<<";"<<obj.getFechaVen()<<";"<<generarSerie(obj.getCodVenta())<<";"<<obj.getTotalVen()<<";"<<obj.getVendedor()<<";"<<endl;
 				archivoVenta.close();					
 			}
 		}
@@ -90,7 +115,7 @@ public:
 			{
 				for(Venta x:vectorVenta)
 				{
-					archivoVenta<<x.getCodVenta()<<";"<<x.getCodCliVen()<<";"<<x.getFechaVen()<<";"<<x.getTotalVen()<<";"<<x.getVendedor()<<";"<<endl;
+					archivoVenta<<x.getCodVenta()<<";"<<x.getCodCliVen()<<";"<<x.getFechaVen()<<";"<<x.getSerieVen()<<";"<<x.getTotalVen()<<";"<<x.getVendedor()<<";"<<endl;
 				}
 				archivoVenta.close();					
 			}
@@ -108,7 +133,7 @@ public:
 			int 	i;
 			size_t 	posi;
 			string 	linea;
-			string 	temporal[5];
+			string 	temporal[6];
 			fstream	archivoVenta;
 			archivoVenta.open("venta.csv",ios::in);
 			if(archivoVenta.is_open())
@@ -124,7 +149,7 @@ public:
 					}
 					
 					Venta objVenta(stoi(temporal[0]),stoi(temporal[1]),
-									   temporal[2],stof(temporal[3]),temporal[4]);
+									   temporal[2],temporal[3],stof(temporal[4]),temporal[5]);
 					
 					add(objVenta);
 				}
@@ -138,3 +163,111 @@ public:
 };
 
 VentaController* ventaController = new VentaController();
+
+void menuDeVentas()
+{
+	int opt;
+	do
+	{
+		cout<<"\n\n\n\n\n\n\n\n";
+		cout<<"\t\t\t\t\t\t*************MENU DE VENTAS************\n\n";
+		cout<<"\t\t\t\t\t\t**********LISTAR VENTAS**************[1]\n\n";
+		cout<<"\t\t\t\t\t\t************BUSCAR VENTAS************[2]\n\n";
+		cout<<"\t\t\t\t\t\t***********ELIMINAR VENTAS***********[3]\n\n";
+		cout<<"\t\t\t\t\t\t****************SALIR****************[4]\n\n";
+		cout<<"\t\t\t\t\t\t***********INGRESE UNA OPCION***********:";
+		cin>>opt;
+		switch(opt)
+		{
+		case 1:system("cls");listarVentas();break;
+		case 2:system("cls");buscarVenta();break;
+		case 3:system("cls");eliminarVenta();break;
+		case 4:cout<<"\t\t\t\t\t\tGracias por usar nuestro programa\n";break;
+		default:system("cls");cout<<"\t\t\t\t\t\tINGRESA UNA OPCION CORRECTA[1-4]\n";
+		}
+	}
+	while(opt!=4);
+}
+
+
+void listarVentas()
+{
+	cout<<"\n\n\n\n\n\n";
+	cout<<"\t|----COD.VENTA----|  |----CLIENTE----|  |----FECHA----|   |----SERIE----|   |----TOTAL---|     |----VENDEDOR---| \n";
+	for(int i = 0;i<ventaController->size();i++)
+	{
+		cout<<"\t\t"<<ventaController->get(i).getCodVenta()<<"     \t\t"<<dameNomCli(ventaController->get(i).getCodCliVen())<<"\t"
+			<<ventaController->get(i).getFechaVen()<<"\t\t"
+			<<ventaController->get(i).getSerieVen()<<"\t\t"
+			<<ventaController->get(i).getTotalVen()<<"\t\t"
+			<<ventaController->get(i).getVendedor()<<endl;
+	}
+	
+	system("pause");
+	system("cls");	
+}
+
+void buscarVenta()
+{
+	int cod,
+		inferior,
+		superior;
+	inferior = 0;
+	superior = ventaController->size()-1;
+	cout<<"\n\n\n\n\n\n\t\t\t\t\t\tIngrese codigo a buscar: ";
+	cin>>cod;
+	Venta venObj = ventaController->getBuscarPorCodigo(cod, inferior, superior);
+	if(venObj.getFechaVen() !="Error")
+	{
+		cout<<"\t\t\t\t\t\tRegistro encontrado!\n\n";
+		cout<<"\t\t\t\t\t\tCodigo de venta: "<<venObj.getCodVenta()<<"\n";
+		cout<<"\t\t\t\t\t\tCliente :"<<dameNomCli(venObj.getCodCliVen())<<"\n";
+		cout<<"\t\t\t\t\t\tFecha: "<<venObj.getFechaVen()<<"\n";
+		cout<<"\t\t\t\t\t\tSerie: "<<venObj.getSerieVen()<<"\n";
+		cout<<"\t\t\t\t\t\tTotal: "<<venObj.getTotalVen()<<"\n";
+		cout<<"\t\t\t\t\t\tVendedor: "<<venObj.getVendedor()<<"\n";
+		
+	}
+	else
+	{
+		cout<<"\t\t\t\t\t\tNo se encontro el registro!\n";
+	}
+	system("pause");
+	system("cls");
+}
+
+
+void eliminarVenta()
+{
+	int cod,
+		inferior,
+		superior;
+	inferior = 0;
+	superior = ventaController->size()-1;
+	cout<<"\n\n\n\n\n\n\t\t\t\t\t\tIngrese codigo a eliminar: ";
+	cin>>cod;
+	Venta venEliminar = ventaController->getBuscarPorCodigo(cod, inferior, superior);
+	if(venEliminar.getFechaVen() != "Error")
+	{
+		ventaController->remove(venEliminar);
+		cout<<"\t\t\t\t\t\tRegistro eliminado satisfactoriamente!!!";
+		/*Grabar en archivo*/
+		ventaController->grabarModificarEliminarArchivoVenta();
+	}
+	else
+	{
+		cout<<"\t\t\t\t\t\tNo se encontro el registro!\n";
+	}
+}
+
+string dameNomCli(int cod)
+{
+	for(int i = 0; i<cliController->size(); i++)
+	{
+		if(cod == cliController->get(i).getCodigo())
+		{
+			return cliController->get(i).getNomCli();
+		}
+	}
+	return "no encontrado";
+}
